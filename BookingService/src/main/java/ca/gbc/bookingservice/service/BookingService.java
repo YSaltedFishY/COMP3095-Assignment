a20f746f91976a54728640dfc01828c75359e498
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-07 20:03:09 -0500

    Added Booking services and implementation

---
+++ BookingService/src/main/java/ca/gbc/bookingservice/service/BookingService.java
@@ -0,0 +1,15 @@
+package ca.gbc.bookingservice.service;
+
+import ca.gbc.bookingservice.dto.BookingRequest;
+import ca.gbc.bookingservice.dto.BookingResponse;
+
+import java.util.List;
+
+public interface BookingService {
+
+    BookingResponse createBooking(BookingRequest bookingRequest);
+
+    List<BookingResponse> getAllBookings(BookingRequest bookingRequest);
+
+
+}
---
+++ BookingService/src/main/java/ca/gbc/bookingservice/service/BookingServiceImpl.java
@@ -0,0 +1,43 @@
+package ca.gbc.bookingservice.service;
+
+import ca.gbc.bookingservice.dto.BookingRequest;
+import ca.gbc.bookingservice.dto.BookingResponse;
+import ca.gbc.bookingservice.model.Booking;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Service;
+import ca.gbc.bookingservice.repository.BookingRepository;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class BookingServiceImpl implements BookingService {
+    private final BookingRepository bookingRepository;
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public BookingResponse createBooking(BookingRequest bookingRequest) {
+       //TODO: Validate
+
+        // Create a new Booking object
+        Booking booking = Booking.builder()
+                .id(bookingRequest.Id())
+                .userId(bookingRequest.UserId())
+                .roomId(bookingRequest.roomId())
+                .startTime(bookingRequest.startTime())
+                .endTime(bookingRequest.endTime())
+                .purpose(bookingRequest.purpose())
+                .build();
+        Booking savedBooking = bookingRepository.save(booking);
+
+        return new BookingResponse(savedBooking.getId(), "SUCCESS", "Booking created successfully.");
+    }
+
+    @Override
+    public List<BookingResponse> getAllBookings(BookingRequest bookingRequest) {
+        return null;
+    }
+}
