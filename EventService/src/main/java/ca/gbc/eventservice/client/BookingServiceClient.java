6cab75769f6a2100860350e9112d66f95e89ac8f
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-09 16:27:05 -0500

    Added feign client

---
+++ EventService/src/main/java/ca/gbc/eventservice/client/BookingServiceClient.java
@@ -0,0 +1,18 @@
+package ca.gbc.eventservice.client;
+
+import ca.gbc.bookingservice.dto.BookingRequest;
+import ca.gbc.bookingservice.dto.BookingResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "BookingService", url = "http://localhost:8080")
+public interface BookingServiceClient {
+    @GetMapping("/api/booking}")
+    ResponseEntity<BookingResponse> createBooking(@RequestBody BookingRequest bookingRequest);
+
+
+}
