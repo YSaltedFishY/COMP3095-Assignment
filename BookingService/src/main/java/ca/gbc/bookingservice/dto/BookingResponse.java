d9ef41c0a5f95f8f1f74f767dc316d99dce0917d
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2024-11-09 11:34:06 -0500

    Fixed data types

--- BookingService/src/main/java/ca/gbc/bookingservice/dto/BookingResponse.java
+++ BookingService/src/main/java/ca/gbc/bookingservice/dto/BookingResponse.java
@@ -3,13 +3,14 @@ package ca.gbc.bookingservice.dto;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public record BookingResponse(
         String bookingId,
         Long UserId,
         Long roomId,
-        LocalDateTime startTime,
-        LocalDateTime endTime,
+        Date startTime,
+        Date endTime,
         String purpose
 ) {
 }
