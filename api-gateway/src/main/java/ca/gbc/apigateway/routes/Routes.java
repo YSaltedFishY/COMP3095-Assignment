6b8e6f7cbc0d8489b736e930d2daaa960fa635b7
YSaltedFishY <yxwyyhy@hotmail.com>
2024-12-02 13:19:11 -0500

    Functional API gateway and keycloak auth in container

--- api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
+++ api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
@@ -33,7 +33,7 @@ public class Routes {
         log.info("Initializing product service route with URL: {}", roomServiceUrl);
 
         return GatewayRouterFunctions.route("room_service")
-                .route(RequestPredicates.path("/api/room/**"), request -> {
+                .route(RequestPredicates.path("/api/room"), request -> {
                     log.info("Received request for product service: {}", request.uri());
                     try{
                         ServerResponse response = HandlerFunctions.http(roomServiceUrl).handle(request);
@@ -43,7 +43,50 @@ public class Routes {
                         log.error("Error occurred while routing request: {}", e.getMessage());
                         return ServerResponse.status(500).body("An error occurred routing");
                     }
-
+                })
+                .route(RequestPredicates.path("/api/room/{id}"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(roomServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+                })
+                .route(RequestPredicates.path("/api/room/available"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(roomServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+                })
+                .route(RequestPredicates.path("/api/room/available/{id}"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(roomServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+                })
+                .route(RequestPredicates.path("/api/room/deleteeverything"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(roomServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
                 }).build();
 
     }
@@ -53,7 +96,19 @@ public class Routes {
         log.info("Initializing product service route with URL: {}", bookingServiceUrl);
 
         return GatewayRouterFunctions.route("booking_service")
-                .route(RequestPredicates.path("/api/booking/**"), request -> {
+                .route(RequestPredicates.path("/api/booking"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(bookingServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+
+                })
+                .route(RequestPredicates.path("/api/booking/{id}"), request -> {
                     log.info("Received request for product service: {}", request.uri());
                     try{
                         ServerResponse response = HandlerFunctions.http(bookingServiceUrl).handle(request);
@@ -73,7 +128,43 @@ public class Routes {
         log.info("Initializing product service route with URL: {}", userServiceUrl);
 
         return GatewayRouterFunctions.route("user_service")
-                .route(RequestPredicates.path("/api/user/**"), request -> {
+                .route(RequestPredicates.path("/api/user"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(userServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+
+                })
+                .route(RequestPredicates.path("/api/user/{id}"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(userServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+
+                })
+                .route(RequestPredicates.path("/api/user/approve/{id}"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(userServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+
+                })
+                .route(RequestPredicates.path("/api/user/deleteeverything"), request -> {
                     log.info("Received request for product service: {}", request.uri());
                     try{
                         ServerResponse response = HandlerFunctions.http(userServiceUrl).handle(request);
@@ -93,7 +184,19 @@ public class Routes {
         log.info("Initializing product service route with URL: {}", eventServiceUrl);
 
         return GatewayRouterFunctions.route("event_service")
-                .route(RequestPredicates.path("/api/event/**"), request -> {
+                .route(RequestPredicates.path("/api/event"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(eventServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+
+                })
+                .route(RequestPredicates.path("/api/event/{id}"), request -> {
                     log.info("Received request for product service: {}", request.uri());
                     try{
                         ServerResponse response = HandlerFunctions.http(eventServiceUrl).handle(request);
@@ -113,7 +216,7 @@ public class Routes {
         log.info("Initializing product service route with URL: {}", approvalServiceUrl);
 
         return GatewayRouterFunctions.route("approval_service")
-                .route(RequestPredicates.path("/api/approval/**"), request -> {
+                .route(RequestPredicates.path("/api/approval"), request -> {
                     log.info("Received request for product service: {}", request.uri());
                     try{
                         ServerResponse response = HandlerFunctions.http(approvalServiceUrl).handle(request);
@@ -123,8 +226,34 @@ public class Routes {
                         log.error("Error occurred while routing request: {}", e.getMessage());
                         return ServerResponse.status(500).body("An error occurred routing");
                     }
-
+                })
+                .route(RequestPredicates.path("/api/approval/{id}"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(approvalServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
+                })
+                .route(RequestPredicates.path("/api/approval/approve"), request -> {
+                    log.info("Received request for product service: {}", request.uri());
+                    try{
+                        ServerResponse response = HandlerFunctions.http(approvalServiceUrl).handle(request);
+                        log.info("Response status: {}", response.statusCode());
+                        return response;
+                    }catch(Exception e){
+                        log.error("Error occurred while routing request: {}", e.getMessage());
+                        return ServerResponse.status(500).body("An error occurred routing");
+                    }
                 }).build();
-
     }
+
+
+
+
+
+
 }
